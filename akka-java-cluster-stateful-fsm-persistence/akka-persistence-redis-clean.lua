3e67260a6a873e7608f0e0f64e990e45542de006
Lei Zhang <zhanglei@apache.org>
2019-08-30 18:03:57 +0800

    init

--- akka-java-cluster-stateful-fsm-persistence/akka-persistence-redis-clean.lua
+++ akka-java-cluster-stateful-fsm-persistence/akka-persistence-redis-clean.lua
@@ -1,27 +1,3 @@
-# 有状态 Actor 在集群中的使用
-
-* 有状态 Actor
-* 持久化 Actor 
-* 集群分片
-* 高可用
-
-![image-20190830135523054](assets/image-20190830135523054.png)
-
-
-
-## 持久化清理
-
-使用 Redis 持久化插件，需要定期手动清理已终止的Actor的数据
-
-清理命令
-
-```bash
-redis-cli --eval akka-persistence-redis-clean.lua
-```
-
-akka-persistence-redis-clean.lua 脚本如下
-
-```lua
 local ids = redis.call('smembers','journal:persistenceIds');
 local delkeys = {};
 for k, v in pairs(ids) do
@@ -43,10 +19,3 @@ for k, v in pairs(ids) do
   end
 end
 return delkeys;
-```
-
-## 重复消费
-
-* Kafka 消费可能存在重复情况，Actor 要自己判断处理
-
- 
